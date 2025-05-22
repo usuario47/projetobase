d2b66e2e4ad9bf2db34784d2b667f185b8a57a04
usuario47 <chaderalemchandre@gmail.com>
2025-05-22 11:23:26 -0300

    aprendi a caucular no js

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,16 @@
-console.log ("oi")
+let number1 = 80
+let number2 = 40
+let som1 = number1+number2
+let som2 = number1-number2
+let som3 = number1/23*number2
+let som4 = number1*number2
+console.log ("a soma é: "+som1)
+console.log("a subtração é: " +som2)
+console.log("a divisão é: "+som3)   
+console.log("a mutiplicação é:" +som4)
+let salario = 1518
+let mestrab = 13;
+let nome = "Richard";
+let decimo = salario/12*mestrab
+console.log("Parabéns "+nome+ "seu 13° é R$"+decimo)
+
