473fd7a00952afda2b6fe89ad89b77dfdefed376
usuario47 <chaderalemchandre@gmail.com>
2025-06-12 11:32:07 -0300

    mudar cor do butão do cadastro 12/06 quinta-feira

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+function clicou(){
+     let quandoclicar = document.getElementById("botao")
+    quandoclicar.innerText ="ERRO"
+}
+function saio(){
+let quandosair = document.getElementById("botao")
+    quandosair.style.background = "green"
+}
+function entrar(){
+let quandoentrar = document.getElementById("botao")
+    quandoentrar.style.background = "red" 
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+/*let  buscando = document.getElementsByTagName('p')[0]
+console.log(buscando)
+let buscando = document.getElementsByName("h2dadiv")[0]
+console.log(buscando)
+let buscando = document.getElementById("Atalaia")
+buscando.style.background = "gray"
+ou : GetElementsClassName()
+let modorapido = document.querySelector("#atalaia")
+modorapido.computedStyleMap.background = "gray"*/
+document.getElementById
